deaefc346e96e67f3e1c1a43aa9434d04532b4c5
koolbasov <kolbasov.anton@gmail.com>
2024-05-23 15:02:39 +0300

    Excercise 3_for.py ready

--- 3_for.py
+++ 3_for.py
@@ -4,9 +4,9 @@
 
 Цикл for: Продажи товаров
 
-* Дан список словарей с данными по колличеству проданных телефонов
+* Дан список словарей с данными по количеству проданных телефонов
   [
-    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
+    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
     {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
     {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
   ]
@@ -16,12 +16,44 @@
 * Посчитать и вывести среднее количество продаж всех товаров
 """
 
+all_phone_sales = [
+    {
+        "product": "iPhone 12",
+        "items_sold": [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186],
+    },
+    {
+        "product": "Xiaomi Mi11",
+        "items_sold": [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316],
+    },
+    {
+        "product": "Samsung Galaxy 21",
+        "items_sold": [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247],
+    },
+]
+
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
+    for product in all_phone_sales:
+        print(
+            f"Cуммарное количество продаж для {product['product']}: {sum(product['items_sold'])}"
+        )
+
+    for product in all_phone_sales:
+        print(
+            f"Среднее количество продаж {product['product']}: {round(sum(product['items_sold']) / len(product['items_sold']), 2)}"
+        )
+
+    all_sales_sum = 0
+    count_sales = 0
+    for product in all_phone_sales:
+        count_sales += len(product["items_sold"])
+        all_sales_sum += sum(product["items_sold"])
+    print(f"Cуммарное количество продаж всех товаров: {all_sales_sum}")
+    print(f"Cреднее количество продаж всех товаров: {all_sales_sum / count_sales}")
+
+
 if __name__ == "__main__":
     main()
